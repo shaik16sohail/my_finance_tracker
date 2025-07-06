14ec7c6fe146703f40262af2b55cc4205291253e
Sohail <143282632+shaik16sohail@users.noreply.github.com>
2025-07-06 23:30:32 +0530

    Update next.config.mjs

--- finance-visualizer/next.config.mjs
+++ finance-visualizer/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+// next.config.js
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+
 
 export default nextConfig;
