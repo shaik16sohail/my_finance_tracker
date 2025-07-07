45130371bf44ccbb605e3d69f0e6eeaeed070719
Sohail <143282632+shaik16sohail@users.noreply.github.com>
2025-07-07 09:25:49 +0530

    Update page.js

--- finance-visualizer/src/app/page.js
+++ finance-visualizer/src/app/page.js
@@ -1,5 +1,8 @@
 import Image from "next/image";
-
+export const metadata = {
+  title: "Dashboard | Personal Finance Tracker",
+  description: "Track your expenses and manage your finances with ease.",
+};
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
